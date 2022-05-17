9bc47e3703a85627913d158d12a8b31cf2e1cb51
Jochen Weile <jochenweile@gmail.com>
2022-05-17 17:08:06 -0400

    allow more runtime

--- pacbioFilterRQ_ctrl.sh
+++ pacbioFilterRQ_ctrl.sh
@@ -6,7 +6,7 @@ JOBS=""
 for INBAM in $BAMS; do
   #submit filter job
   echo "Processing $INBAM"
-  RETVAL=$(submitjob.sh -c 4 -m 4G -- pacbioFilterRQ.sh "$INBAM")
+  RETVAL=$(submitjob.sh -c 4 -m 4G -t 1-00:00:00 -- pacbioFilterRQ.sh "$INBAM")
   JOBID=${RETVAL##* }
     if [ -z "$JOBS" ]; then
       #if jobs is empty, set it to the new ID
